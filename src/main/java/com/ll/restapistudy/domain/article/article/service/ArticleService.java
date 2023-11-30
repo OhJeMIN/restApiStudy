ceade8e33205fc6ef0a4289ff045e5803d8c481d
jasonoh22 <jasonoh22@naver.com>
2023-11-30 13:56:13 +0900

    feat: Article, Member, global setting

---
+++ src/main/java/com/ll/restapistudy/domain/article/article/service/ArticleService.java
@@ -0,0 +1,42 @@
+package com.ll.restapistudy.domain.article.article.service;
+
+import com.ll.restapistudy.domain.article.article.entity.Article;
+import com.ll.restapistudy.domain.article.article.repository.ArticleRepository;
+import com.ll.restapistudy.domain.member.member.entity.Member;
+import com.ll.restapistudy.global.raData.RsData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    @Transactional
+    public RsData<Article> write(Member author, String title, String body) {
+        Article article = Article.builder()
+                .modifyDate(LocalDateTime.now())
+                .author(author)
+                .title(title)
+                .body(body)
+                .build();
+
+        articleRepository.save(article);
+
+        return RsData.of("200", "%d번 게시글이 작성되었습니다.".formatted(article.getId()), article);
+    }
+
+    public Optional<Article> findById(long id) {
+        return articleRepository.findById(id);
+    }
+
+    @Transactional
+    public void modify(Article article, String title, String body) {
+        article.setTitle(title);
+        article.setBody(body);
+    }
+}
---
+++ src/main/java/com/ll/restapistudy/domain/member/member/entity/Member.java
@@ -0,0 +1,24 @@
+package com.ll.restapistudy.domain.member.member.entity;
+
+import com.ll.restapistudy.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@SuperBuilder
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+@Setter
+@Getter
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
+    private String username;
+    private String password;
+    private String email;
+    private String nickname;
+}
