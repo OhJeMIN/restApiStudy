086219c4a5fd843082cf0d2d714847c8a651a8b7
jasonoh22 <jasonoh22@naver.com>
2023-11-30 13:23:57 +0900

    settings 2

--- src/test/java/com/ll/restapistudy/RestApiStudyApplicationTests.java
+++ src/test/java/com/ll/restapistudy/RestApiStudyApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.restapistudy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RestApiStudyApplicationTests {
 
     @Test
